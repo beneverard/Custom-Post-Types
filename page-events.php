f78d1f258797610b055e5752e627e35f3d24d18e
Ben Everard <hello@beneverard.co.uk>
2011-12-13 21:26:33 +0000

    Added events page, code to display last 5 events

---
+++ page-events.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<?php
+
+	// for our custom loop, get the last 5 event posts
+	$the_query = new WP_Query(
+		array(
+			'post_type'		=> 'event',
+			'posts_per_page'	=> '5'
+		)
+	);
+
+?>
+		
+<?php if ($the_query->have_posts()) while ($the_query->have_posts()) : $the_query->the_post(); ?>
+
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+	<?php the_excerpt(); ?>
+	
+<?php endwhile; ?>
+
+<?php wp_reset_postdata(); ?>
+
+<?php get_footer(); ?>
